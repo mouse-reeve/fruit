709b72d6324eb6cc7f675a998ec0119e5e1918b5
Mouse Reeve <mousereeve@riseup.net>
2018-10-01 15:59:24 -0700

    Semi-functional symmetry

--- draw.js
+++ draw.js
@@ -28,43 +28,63 @@ function setup() {
     noLoop();
 }
 
+var radius_base = 100;
 function draw() {
     // draw a fruit centered at x,y
     x = 500;
     y = 250;
 
-    var radius_base = 100;
     var radius_y = radius_base;
     var radius_x = radius_base;
     var perturbation_y = 10;
     var perturbation_x = 25;
-    var points = Math.round(random(3, 10));
+    var points = 5;//Math.round(random(2, 5)) * 2;
+    var control_radius = 120 / points;
 
-    outline = [];
     var radii = [];
-    for (var a = HALF_PI + (PI / 20); a < 3 * HALF_PI; a += PI / points) {
-        radius_x += Math.round(random(-1 * perturbation_x, perturbation_x));
-        radius_y += Math.round(random(-1 * perturbation_y, perturbation_y));
-        radii.push([radius_x, radius_y]);
+    var outline = [];
+    var center_offset = PI / 20;
+    var angle = PI / points;
+    for (var a = HALF_PI + center_offset; a < 5 * HALF_PI; a += angle) {
+        if (a < 3 * HALF_PI) {
+            radii.push(radius_y);
+            radii.push(radius_x);
+        } else {
+            radius_x = radii.pop();
+            radius_y = radii.pop();
+        }
+        var sx1 = x + cos(a) * radius_x;
+        var sy1 = y - sin(a) * radius_y;
 
-        var sx = x + cos(a) * radius_x;
-        var sy = y - sin(a) * radius_y;
-        outline.push([sx, sy]);
+        var cx1 = sx1 + cos(a + HALF_PI) * control_radius;
+        var cy1 = sy1 - sin(a + HALF_PI) * control_radius;
+
+        if (a < 3 * HALF_PI - angle) {
+            radius_x += Math.round(random(-1 * perturbation_x, perturbation_x));
+            radius_y += Math.round(random(-1 * perturbation_y, perturbation_y));
+            radii.push(radius_y);
+            radii.push(radius_x);
+        } else {
+            radius_x = radii.pop() + random(-3, 3);
+            radius_y = radii.pop() + random(-3, 3);
+        }
+        var sx2 = x + cos(a + angle) * radius_x;
+        var sy2 = y - sin(a + angle) * radius_y;
+
+        var cx2 = sx2 - cos(a + angle + HALF_PI) * control_radius;
+        var cy2 = sy2 + sin(a + angle + HALF_PI) * control_radius;
+
+        var outline_point = [
+            cx1, cy1,
+            cx2, cy2,
+            sx2, sy2
+        ];
+        // stash this so I can make concentric shapes
+        outline_point.radius = [radius_x, radius_y];
+        outline_point.angle = a;
+        outline.push(outline_point);
     }
 
-    var radii_cont = [];
-    var idx = 0;
-    for (var a = HALF_PI - (PI / 20); a > -1 * HALF_PI; a -= PI / points) {
-        r = radii[idx];
-        radius_x = r[0];
-        radius_y = r[1];
-        radii_cont.push(r);
-
-        var sx = x + cos(a) * radius_x;
-        var sy = y - sin(a) * radius_y;
-        outline.splice(points, 0, [sx, sy]);
-        idx++;
-    }
 
     push();
     stroke(black);
@@ -72,10 +92,16 @@ function draw() {
     beginShape();
     noFill();
     vertex(x, y - radius_base);
-
     for (var v = 0; v < outline.length; v++) {
-        vertex(outline[v][0], outline[v][1]);
+        bezierVertex(...outline[v]);
+        //ellipse(outline[v][4], outline[v][5], 5, 5);
+        /*push();
+        fill(black);
+        ellipse(outline[v][0], outline[v][1], 2, 2);
+        ellipse(outline[v][2], outline[v][3], 2, 2);
+        pop();*/
     }
     endShape(CLOSE);
     pop();
 }
+
