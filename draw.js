4c26def508f6f3727a8a7ae01ac6ba93fd134029
Mouse Reeve <mousereeve@riseup.net>
2018-10-01 17:46:41 -0700

    Uses translation and scale

--- draw.js
+++ draw.js
@@ -10,7 +10,7 @@ function setup() {
     console.log(seed);
 
     var container = document.getElementById('fruit');
-    var canvas = createCanvas(1000, 600);
+    var canvas = createCanvas(500, 600);
     canvas.parent(container);
 
     black = color(0);
@@ -22,7 +22,6 @@ function setup() {
     push();
     pop();
     stroke(black);
-    fill(white);
     rect(0, 0, width - 1, height -1);
 
     noLoop();
@@ -30,78 +29,114 @@ function setup() {
 
 var radius_base = 100;
 function draw() {
-    // draw a fruit centered at x,y
-    x = 500;
-    y = 250;
+    // draw a fruit
+    x = 0;
+    y = 0;
 
     var radius_y = radius_base;
     var radius_x = radius_base;
     var perturbation_y = 10;
     var perturbation_x = 25;
-    var points = 5;//Math.round(random(2, 5)) * 2;
+    var points = Math.round(random(4, 7));
     var control_radius = 120 / points;
 
     var radii = [];
-    var outline = [];
-    var center_offset = PI / 20;
     var angle = PI / points;
-    for (var a = HALF_PI + center_offset; a < 5 * HALF_PI; a += angle) {
-        if (a < 3 * HALF_PI) {
-            radii.push(radius_y);
-            radii.push(radius_x);
-        } else {
-            radius_x = radii.pop();
-            radius_y = radii.pop();
-        }
+    var base_shape = [];
+    for (var a = HALF_PI; a < 3 * HALF_PI; a += angle) {
         var sx1 = x + cos(a) * radius_x;
         var sy1 = y - sin(a) * radius_y;
 
         var cx1 = sx1 + cos(a + HALF_PI) * control_radius;
         var cy1 = sy1 - sin(a + HALF_PI) * control_radius;
 
-        if (a < 3 * HALF_PI - angle) {
-            radius_x += Math.round(random(-1 * perturbation_x, perturbation_x));
-            radius_y += Math.round(random(-1 * perturbation_y, perturbation_y));
-            radii.push(radius_y);
-            radii.push(radius_x);
-        } else {
-            radius_x = radii.pop() + random(-3, 3);
-            radius_y = radii.pop() + random(-3, 3);
-        }
+        radius_x += Math.round(random(-1 * perturbation_x, perturbation_x));
+        radius_y += Math.round(random(-1 * perturbation_y, perturbation_y));
+        radii.push(radius_y);
+        radii.push(radius_x);
+
         var sx2 = x + cos(a + angle) * radius_x;
         var sy2 = y - sin(a + angle) * radius_y;
 
         var cx2 = sx2 - cos(a + angle + HALF_PI) * control_radius;
         var cy2 = sy2 + sin(a + angle + HALF_PI) * control_radius;
 
-        var outline_point = [
+        var base_shape_point = [
             cx1, cy1,
             cx2, cy2,
             sx2, sy2
         ];
         // stash this so I can make concentric shapes
-        outline_point.radius = [radius_x, radius_y];
-        outline_point.angle = a;
-        outline.push(outline_point);
+        base_shape_point.radius = [radius_x, radius_y];
+        base_shape_point.angle = a;
+        base_shape.push(base_shape_point);
     }
 
-
+    noStroke();
+    translate(250, 150);
+    // outside
     push();
-    stroke(black);
+    scale(-1, 1);
+    fill('#f00');
+    draw_shape(base_shape);
+    pop();
 
-    beginShape();
-    noFill();
-    vertex(x, y - radius_base);
-    for (var v = 0; v < outline.length; v++) {
-        bezierVertex(...outline[v]);
-        //ellipse(outline[v][4], outline[v][5], 5, 5);
-        /*push();
-        fill(black);
-        ellipse(outline[v][0], outline[v][1], 2, 2);
-        ellipse(outline[v][2], outline[v][3], 2, 2);
-        pop();*/
-    }
-    endShape(CLOSE);
+    // shadow
+    push();
+    scale(0.4, 0.4);
+    translate(-50, -30);
+    fill(100, 100, 100, 40);
+    draw_shape(base_shape, -0.3);
+    pop();
+
+    translate(0, 250);
+
+    // peel
+    push();
+    fill(black);
+    draw_shape(base_shape);
+    pop();
+
+    var running_scale = 0.98;
+    // pith
+    push();
+    scale(running_scale, running_scale);
+    fill('#b00');
+    draw_shape(base_shape);
+    pop();
+
+    // meat
+    running_scale -= 0.05;
+    push();
+    scale(running_scale, running_scale);
+    fill('#f88');
+    draw_shape(base_shape);
+    pop();
+
+    // pit
+    push();
+    scale(0.3, 0.7);
+    fill('#500');
+    draw_shape(base_shape);
     pop();
 }
 
+function draw_shape(vertices, x_scale_factor) {
+    x_scale_factor = x_scale_factor || -1;
+    for (var i = 0; i < 2; i++) {
+        beginShape();
+        vertex(x, y - radius_base);
+
+        for (var v = 0; v < vertices.length; v++) {
+            bezierVertex(...vertices[v]);
+            //ellipse(vertices[v][4], vertices[v][5], 5, 5);
+            /*push();
+            fill(black);
+            ellipse(vertices[v][0], vertices[v][1], 2, 2);
+            ellipse(vertices[v][2], vertices[v][3], 2, 2);
+            pop();*/
+        }
+        endShape(CLOSE);
+        scale(x_scale_factor, 1);
+    }
+}
